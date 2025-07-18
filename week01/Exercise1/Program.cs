a1fcfc7fdf616cfe73ef66a36186261465261591
Jonesgolo <jonesgolo@gmail.com>
2025-07-18 19:22:35 +0100

    Week 1 Assignment updated

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        
+        Console.Write("What is your firstname: ");
+        string firstname = Console.ReadLine();
+
+        Console.Write("What is your lastname: ");
+        string lastname = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lastname}, {firstname}, {lastname}");
     }
 }
